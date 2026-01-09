ccba905ebd7c26d965ab24b8b13bb77ac131ac52
Noa Houssier <noahoussier@gmail.com>
2026-01-09 09:26:32 +0100

    refactor: move init_stack tests to own function

--- tests/src/stack.c
+++ tests/src/stack.c
@@ -1,27 +1,32 @@
+#include "stack.h"
 #include "libft.h"
 #include "libtests.h"
-#include "stack.h"
 
 #define SIZE 42
 
-int	main(void)
+void	test_init_stack(void)
 {
-	int		errors;
-	t_stack	*stack;
-	
-	errors = 0;
-	stack = init_stack(SIZE);
-	errors += expect_not_null(stack, "init_stack (returns a new stack)");
-	errors += expect_not_null(stack->content,
-			"init_stack (inits stack's content)");
-	errors += expect_eq_int(stack->size, SIZE,
-			"init_stack (sets stack's size to the given size)");
-	errors += expect_eq_int(stack->top, 0,
-			"init_stack (sets stack's top to 0)");
-	errors += expect_eq_int(stack->bottom, 0,
-			"init_stack (sets stack's bottom to 0)");
-	free_stack(stack);
-	if (errors == 0)
-		ft_printf("\ninit_stack: Success\n");
+    int		errors;
+    t_stack	*stack;
+
+    errors = 0;
+    stack = init_stack(SIZE);
+    errors += expect_not_null(stack, "init_stack (returns a new stack)");
+    errors += expect_not_null(stack->content,
+		"init_stack (inits stack's content)");
+    errors += expect_eq_int(stack->size, SIZE,
+		"init_stack (sets stack's size to the given size)");
+    errors += expect_eq_int(stack->top, 0,
+		"init_stack (sets stack's top to 0)");
+    errors += expect_eq_int(stack->bottom, 0,
+		"init_stack (sets stack's bottom to 0)");
+    free_stack(stack);
+    if (errors == 0)
+        ft_printf("\ninit_stack: Success\n");
+}
+
+int		main(void)
+{
+    test_init_stack();
 	return (0);
 }
