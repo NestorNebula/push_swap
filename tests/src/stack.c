eeb4ecd00cf671dba1b7e5126c4f5f246acf66b6
Noa Houssier <noahoussier@gmail.com>
2026-01-12 12:41:28 +0100

    test: add tests for is_sorted_stack function

--- tests/src/stack.c
+++ tests/src/stack.c
@@ -1,9 +1,11 @@
+#include "stack.h"
 #include "parse.h"
 #include "libft.h"
 #include "libtests.h"
 
 #define SIZE 5
 #define ARGS (const char *[]){"1", "2", "3", "4", "5"}
+#define REV_ARGS (const char *[]){"5", "4", "3", "2", "1"}
 #define INT_TO_PUSH 42
 
 void	test_init_stack(void)
@@ -110,11 +112,42 @@ void	test_get_stack_len(void)
 		ft_printf("\nget_stack_len: Success\n");
 }
 
+void	test_is_sorted_stack(void)
+{
+	int		errors;
+	t_stack	*stack;
+
+	errors = 0;
+	stack = init_stack(SIZE);
+	errors += expect_not_null(stack,
+		"is_sorted_stack (stack initialization error)");
+	(void) parse_args(ARGS, SIZE, stack);
+	errors += expect_true(is_sorted_stack(stack, true),
+		"is_sorted_stack (returns true for sorted stack in ascending order)");
+	errors += expect_false(is_sorted_stack(stack, false),
+		"is_sorted_stack (returns false for unsorted stack in ascending order)");
+	stack->content[stack->top - 1] += 2;
+	errors += expect_false(is_sorted_stack(stack, true),
+		"is_sorted_stack (returns false for unsorted stack in ascending order)");
+	(void) parse_args(REV_ARGS, SIZE, stack);
+	errors += expect_true(is_sorted_stack(stack, false),
+		"is_sorted_stack (returns true for sorted stack in descending order)");
+	errors += expect_false(is_sorted_stack(stack, true),
+		"is_sorted_stack (returns false for unsorted stack in descending order)");
+	stack->content[stack->top - 1] -= 2;
+	errors += expect_false(is_sorted_stack(stack, false),
+		"is_sorted_stack (returns false for unsorted stack in descending order)");
+	free_stack(stack);
+	if (errors == 0)
+		ft_printf("\nis_sorted_stack: Success\n");
+}
+
 int		main(void)
 {
     test_init_stack();
 	test_push_stack();
 	test_pop_stack();
 	test_get_stack_len();
+	test_is_sorted_stack();
 	return (0);
 }
