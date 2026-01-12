dbf0e7272dd1424e5fffeaa772c30fc4446fe702
Noa Houssier <nestornebula.dev@gmail.com>
2026-01-12 19:24:19 +0100

    feat: add new stack helpers functions

--- src/stack-helpers.c
+++ src/stack-helpers.c
@@ -49,3 +49,80 @@ bool		is_sorted_stack(t_stack *stack, bool asc)
 	}
 	return (true);
 }
+
+uint64_t	find_maximum_smaller(t_stack *stack, int n)
+{
+	int			*max;
+	int			*max_smaller;
+	uint64_t	i;
+
+	if (stack == NULL || get_stack_len(stack) == 0)
+		return (0);
+	if (stack->top == 0)
+		stack->top = stack->size;
+	i = stack->bottom;
+	max = stack->content + i;
+	max_smaller = NULL;
+	while (i != stack->top)
+	{
+		if (i == stack->size)
+			i = 0;
+		if (stack->content[i] > *max)
+			max = stack->content + i;
+		if (stack->content[i] < n
+			&& (max_smaller == NULL || stack->content[i] > *max_smaller))
+			max_smaller = stack->content + i;
+		i++;
+	}
+	if (max_smaller != NULL)
+		return (max_smaller - stack->content);
+	return (max - stack->content);
+}
+
+uint64_t	find_minimum_bigger(t_stack *stack, int n)
+{
+	int			*min;
+	int			*min_bigger;
+	uint64_t	i;
+
+	if (stack == NULL || get_stack_len(stack) == 0)
+		return (0);
+	if (stack->top == 0)
+		stack->top = stack->size;
+	i = stack->bottom;
+	min = stack->content + i;
+	min_bigger = NULL;
+	while (i != stack->top)
+	{
+		if (i == stack->size)
+			i = 0;
+		if (stack->content[i] < *min)
+			min = stack->content + i;
+		if (stack->content[i] > n
+			&& (min_bigger == NULL || stack->content[i] < *min_bigger))
+			min_bigger = stack->content + i;
+		i++;
+	}
+	if (min_bigger != NULL)
+		return (min_bigger - stack->content);
+	return (min - stack->content);
+}
+
+int			gap_to_top(t_stack *stack, uint64_t index, bool allow_bottom)
+{
+	uint64_t	v_index;
+	uint64_t	v_top_index;
+
+	if (stack == NULL || get_stack_len(stack) == 0)
+		return (0);
+	v_index = index;
+	if (index < stack->bottom)
+		v_index = stack->size + index;
+	v_top_index = stack->top;
+	if (stack->top < stack->bottom)
+		v_top_index = stack->size + stack->top;
+	v_top_index--;
+	if (v_top_index - v_index < v_index - stack->bottom || !allow_bottom)
+		return (v_top_index - v_index);
+	return (stack->bottom - v_index - 1);
+}
