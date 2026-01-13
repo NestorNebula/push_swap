9f142b80b02d78b792f6fdd51a3d4f1676e956f9
Noa Houssier <nestornebula.dev@gmail.com>
2026-01-13 11:47:26 +0100

    feat: add prototypes for sort helpers functions

--- include/sort.h
+++ include/sort.h
@@ -24,4 +24,39 @@
  */
 void	sort(t_stack *stack_a, t_stack *stack_b);
 
+/**
+ * Calls an operation function on a stack structure,
+ * printing the executed operation.
+ *
+ * @param stack The stack on which the operation will be executed
+ * @param op A pointer to an operation function
+ * @param op_str The string that will be printed after operation execution
+ */
+void	do_stack_op(t_stack *stack,
+		void (*op)(t_stack *), const char *op_str);
+
+/**
+ * Calls an operation function on two stack structures,
+ * printing the executed operation.
+ *
+ * @param stack_a The first stack on which the operation will be executed
+ * @param stack_b The second stack on which the operation will be executed
+ * @param op A pointer to an operation function
+ * @param op_str The string that will be printed after operation executions
+ */
+void	do_both_stack_op(t_stack *stack_a, t_stack *stack_b,
+		void (*op)(t_stack *), const char *op_str);
+
+/**
+ * Calls the push operation with a source and a destination stack,
+ * printing the executed operation.
+ *
+ * @param src_stack The stack that will be used as source stack
+ * @param dest_stack The stack that will be used as destination stack
+ * @param push A pointer to a push operation function
+ * @param op_str The string that will be printed after operation execution
+ */
+void	do_push(t_stack *src_stack, t_stack *dest_stack,
+		void (*push)(t_stack *, t_stack *), const char *op_str);
+
 #endif // !SORT_H
