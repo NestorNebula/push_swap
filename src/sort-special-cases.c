39da33b44f11e82180a91a4abf3aba3523163e3e
Noa Houssier <nestornebula.dev@gmail.com>
2026-01-14 08:27:06 +0100

    refactor: move sort three functions to file

---
+++ src/sort-special-cases.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort-special-cases.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/14 08:25:41 by nhoussie          #+#    #+#             */
+/*   Updated: 2026/01/14 08:26:58 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "operations.h"
+#include "sort.h"
+
+static void			handle_three_cases(t_stack *stack,
+		uint64_t b, uint64_t m, uint64_t t);
+
+void	sort_three(t_stack *stack)
+{
+	uint64_t	middle;
+	uint64_t	top;
+
+	middle = stack->bottom + 1;
+	if (middle == stack->size)
+		middle = 0;
+	top = stack->top - 1;
+	if (stack->top == 0)
+		middle = stack->size - 1;
+	handle_three_cases(stack, stack->bottom, middle, top);
+}
+
+static void			handle_three_cases(t_stack *stack,
+		uint64_t b, uint64_t m, uint64_t t)
+{
+	const int *c = stack->content;
+	
+	if (c[b] < c[m] && c[m] > c[t] && c[b] < c[t])
+		do_stack_op(stack, op_reverse_rotate, "rra");
+	else if (c[b] > c[m] && c[m] < c[t] && c[b] < c[t])
+		do_stack_op(stack, op_rotate, "ra");
+	else if (c[b] < c[m] && c[m] > c[t] && c[b] > c[t])
+	{
+		do_stack_op(stack, op_swap, "sa");
+		do_stack_op(stack, op_rotate, "ra");
+	}
+	else if (c[b] > c[m] && c[m] < c[t] && c[b] > c[t])
+		do_stack_op(stack, op_swap, "sa");
+	else if (c[b] < c[m] && c[m] < c[t])
+	{
+		do_stack_op(stack, op_swap, "sa");
+		do_stack_op(stack, op_reverse_rotate, "rra");
+	}
+}
--- src/sort.c
+++ src/sort.c
@@ -6,19 +6,11 @@
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 11:52:56 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/01/13 15:39:31 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/01/14 08:26:55 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "operations.h"
-#include <stdint.h>
 #include "sort.h"
-
-static void			handle_three_cases(t_stack *stack,
-		uint64_t b, uint64_t m, uint64_t t);
-
-static void			sort_three(t_stack *stack);
-
 
 
 void	sort(t_stack *stack_a, t_stack *stack_b)
@@ -29,41 +21,4 @@ void	sort(t_stack *stack_a, t_stack *stack_b)
 	// Sort stack a
 	// Push numbers to stack a until size b == 0
 	// Rotate stack a until sorted
-}
-
-static void			handle_three_cases(t_stack *stack,
-		uint64_t b, uint64_t m, uint64_t t)
-{
-	const int *c = stack->content;
-	
-	if (c[b] < c[m] && c[m] > c[t] && c[b] < c[t])
-		do_stack_op(stack, op_reverse_rotate, "rra");
-	else if (c[b] > c[m] && c[m] < c[t] && c[b] < c[t])
-		do_stack_op(stack, op_rotate, "ra");
-	else if (c[b] < c[m] && c[m] > c[t] && c[b] > c[t])
-	{
-		do_stack_op(stack, op_swap, "sa");
-		do_stack_op(stack, op_rotate, "ra");
-	}
-	else if (c[b] > c[m] && c[m] < c[t] && c[b] > c[t])
-		do_stack_op(stack, op_swap, "sa");
-	else if (c[b] < c[m] && c[m] < c[t])
-	{
-		do_stack_op(stack, op_swap, "sa");
-		do_stack_op(stack, op_reverse_rotate, "rra");
-	}
-}
-
-static void			sort_three(t_stack *stack)
-{
-	uint64_t	middle;
-	uint64_t	top;
-
-	middle = stack->bottom + 1;
-	if (middle == stack->size)
-		middle = 0;
-	top = stack->top - 1;
-	if (stack->top == 0)
-		middle = stack->size - 1;
-	handle_three_cases(stack, stack->bottom, middle, top);
 }
